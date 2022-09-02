55dbdd3eeaf2a674bc6762b8f4f783d9d2841b27
Sejin Park <sejpark@student.42seoul.kr>
2022-09-02 19:47:34 +0900

    style : Edit printAPIInfo function

--- Base/include/Base/utility.h
+++ Base/include/Base/utility.h
@@ -116,11 +116,11 @@ std::filesystem::path home();
 
 template<typename App>
 void printAPIInfo(App &app) {
-    std::cout << "EGL_VERSION: " << eglQueryString(app.display, EGL_VERSION) << std::endl;
-    std::cout << "GL_VENDOR: " <<  glGetString(GL_VENDOR) << std::endl;
-    std::cout << "GL_RENDERER: " << glGetString(GL_RENDERER) << std::endl;
-    std::cout << "GL_VERSION: " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GL_SHADING_LANGUAGE_VERSION: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    spdlog::info("EGL Version: {}", eglQueryString(app.display, EGL_VERSION));
+    spdlog::info("GL Vendor: {}", (const char*)glGetString(GL_VENDOR));
+    spdlog::info("GL Renderer: {}", (const char*)glGetString(GL_RENDERER));
+    spdlog::info("GL Version: {}", (const char*)glGetString(GL_VERSION));
+    spdlog::info("GLSL Version: {}", (const char*)glGetString(GL_SHADING_LANGUAGE_VERSION));
 }
 
 #endif //UTILITY_H
